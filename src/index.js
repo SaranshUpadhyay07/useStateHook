4fd6f58b4127f1c954b494dde959b4c83839492e
Saransh Upadhyay <142744774+SaranshUpadhyay07@users.noreply.github.com>
2025-06-18 15:30:02 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./../public/styles.css";
+import App from "./components/App";
+
+createRoot(document.getElementById("root")).render(<App />);
